65f5533e23d77ddda8d07aac3386cf1af02d16dc
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-06-07 17:19:56 +0100

    Add tests

--- test/analytics.test.js
+++ test/analytics.test.js
@@ -15,12 +15,20 @@ describe('Analytics', () => {
 		jest.resetAllMocks()
 	});
 
+	describe('ga', () => {
+		it('should return a view function', () => {
+			const analytics = new Analytics({ propertySettings: [{id: '123', key: 'fakeKey'}], sessionIdProducer: () => '123' })
+			expect(analytics.ga()).toBeDefined()
+			expect(typeof analytics.ga().view).toBe('function')
+		})
+	});
+
 	describe('view', () => {
 		it('should send a view event', async () => {
 			const mockRes = { status: 204 }
 			fetch.mockResolvedValueOnce(() => Promise.resolve | (mockRes))
 			const propertySettings = [ { id: 'testProperty', key: 'testSecret', hitTypes: [ 'pageview' ] } ]
-			const analyticsURI = `https://www.google-analytics.com/mp/collect?measurement_id=${propertySettings[ 0 ].id}&api_secret=${propertySettings[ 0 ].key}`
+			const analyticsURI = `https://www.google-analytics.com/mp/collect?measurement_id=${propertySettings[0].id}&api_secret=${propertySettings[0].key}`
 			const sessionIdProducer = jest.fn(() => '123')
 			const params = { name: 'pageview', params: { page_path: '/test', page_title: 'test' } }
 			const request = {}
