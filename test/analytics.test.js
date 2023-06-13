7d9abab58f44c1808e358816dcad0c30088871b0
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-06-13 12:14:49 +0100

    Bugfix/experiment 03 (#104)
    * Better tests :tada:
    * Add assertions verification to async tests
    * Clean up deps
    * Handle code smells

--- test/analytics.test.js
+++ test/analytics.test.js
@@ -1,6 +1,8 @@
 const Analytics = require('../lib/analytics')
+// const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+// const mockFetch = jest.fn().mockResolvedValueOnce({ status: 204, statusText: 'OK' })
 const fetch = require('node-fetch')
-jest.mock('node-fetch', () => jest.fn())
+jest.mock('node-fetch')
 
 describe('Analytics', () => {
   beforeEach(() => {
@@ -24,50 +26,105 @@ describe('Analytics', () => {
 
   describe('view', () => {
     it('should send a view event', async () => {
-      const mockRes = { status: 204 }
-      fetch.mockResolvedValueOnce(() => Promise.resolve | (mockRes))
+      expect.assertions(3) // -- Important! This ensures that assertions in the async function are run - Update if you add more assertions
+      const mockRes = { status: 204, statusText: 'OK' }
+      fetch.mockResolvedValueOnce(mockRes)
+      const logSpy = jest.spyOn(console, 'log')
       const propertySettings = [{ id: 'testProperty', key: 'testSecret', hitTypes: ['pageview'] }]
       const analyticsURI = `https://www.google-analytics.com/mp/collect?measurement_id=${propertySettings[0].id}&api_secret=${propertySettings[0].key}`
       const sessionIdProducer = jest.fn(() => '123')
-      const params = { name: 'pageview', params: { page_path: '/test', page_title: 'test' } }
+      const metrics = [{ name: 'pageview', params: { page_path: '/test', page_title: 'test' } }]
       const request = {}
       const analytics = new Analytics({ propertySettings, sessionIdProducer })
 
-      await analytics.view(request, params)
+      await analytics.view(request, metrics)
       expect(fetch).toHaveBeenCalledWith(
         analyticsURI,
         {
-          body: '{"client_id":"123","user_id":"123","events":{"name":"pageview","params":{"page_path":"/test","page_title":"test"}}}',
+          body: JSON.stringify({
+            client_id: '123',
+            user_id: '123',
+            events: [{ name: 'pageview', params: { page_path: '/test', page_title: 'test' } }],
+          }),
           method: 'POST'
-        })
+        }
+      )
+      expect(logSpy).toHaveBeenCalledTimes(1)
+      expect(logSpy.mock.calls[0]).toStrictEqual(["Completed request to google analytics measurement protocol API", 204, 'OK'])
     })
 
     it('should NOT send an event if propertySettings are missing', async () => {
+      expect.assertions(1)
       const mockRes = { status: 204 }
-      fetch.mockResolvedValueOnce(() => Promise.resolve | (mockRes))
+      fetch.mockResolvedValueOnce(mockRes)
       const propertySettings = []
       const sessionIdProducer = jest.fn(() => '123')
-      const params = { name: 'pageview', params: { page_path: '/test', page_title: 'test' } }
+      const events = [{ name: 'pageview', params: { page_path: '/test', page_title: 'test' } }]
       const request = {}
       const analytics = new Analytics({ propertySettings, sessionIdProducer })
 
-      await analytics.view(request, params)
+      await analytics.view(request, events)
       expect(fetch).toHaveBeenCalledTimes(0)
     })
+  })
 
-    it('error handling', async () => {
-      fetch.mockRejectedValueOnce(new Error('A7a'))
-      const propertySettings = []
-      const sessionIdProducer = jest.fn(() => '123')
+  describe('sendEvent', () => {
+    const events = ['event1', 'event2'];
+    const measurementId = 'your-measurement-id';
+    const apiSecret = 'your-api-secret';
+    const sessionId = 'your-session-id';
+    const sessionIdProducer = jest.fn(() => '123')
+    const propertySettings = [{ id: measurementId, key: apiSecret, hitTypes: ['pageview'] }]
+    const analyticsURI = `https://www.google-analytics.com/mp/collect?measurement_id=${measurementId}&api_secret=${apiSecret}`
+    it('should send an event to Google Analytics', async () => {
+      expect.assertions(3)
+      const mockRes = { status: 204, statusText: 'OK' }
+      fetch.mockResolvedValueOnce(mockRes)
+      const logSpy = jest.spyOn(console, 'log')
+
       const analytics = new Analytics({ propertySettings, sessionIdProducer })
 
-      try {
-        await analytics.sendEvent([ {} ], '123', '123', 'sessionId');
-      } catch (e) {
-        console.log(e)
-        expect(e).toBeInstanceOf(Error)
-        expect(e.message).toBe('A7a')
-      }
-    })
-  })
+      await analytics.sendEvent(events, measurementId, apiSecret, sessionId);
+      expect(fetch).toHaveBeenCalledWith(
+        analyticsURI,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            client_id: sessionId,
+            user_id: sessionId,
+            events: ['event1', 'event2'],
+          })
+        }
+      )
+      expect(logSpy).toHaveBeenCalledTimes(1);
+      expect(logSpy).toHaveBeenCalledWith(
+        'Completed request to google analytics measurement protocol API',
+        204,
+        'OK'
+      );
+    });
+
+    it('error handling', async () => {
+      expect.assertions(3)
+      fetch.mockRejectedValueOnce({ response: { status: 404, statusText: 'Not Found' } });
+      const logSpy = jest.spyOn(console, 'error')
+
+      const analytics = new Analytics({ propertySettings, sessionIdProducer })
+
+      await analytics.sendEvent(events, measurementId, apiSecret, sessionId);
+      expect(fetch).toHaveBeenCalledWith(
+        analyticsURI,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            client_id: sessionId,
+            user_id: sessionId,
+            events: ['event1', 'event2'],
+          })
+        }
+      )
+      expect(logSpy).toHaveBeenCalledTimes(1);
+      expect(logSpy).toHaveBeenCalledWith("Error sending GA request:", { "response": { "status": 404, "statusText": "Not Found" } });
+    });
+  });
 })
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 
 const indexPlugin = require('../lib/index')
 const analytics = require('../lib/analytics')
-jest.mock('node-fetch', () => jest.fn())
+jest.mock('axios')
 jest.mock('../lib/analytics')
 
 describe('Index', () => {
@@ -17,124 +17,75 @@ describe('Index', () => {
   })
 
   describe('register', () => {
-    it('should throw an error if the provided options are incorrect', async () => {
+    const viewSpy = jest.fn().mockImplementation(() => {
+      return Promise.resolve()
+    })
+    const options = {
+      trackAnalytics: async () => false,
+      propertySettings: [
+        {
+          key: 'superSecretKey',
+          id: 'FakeId',
+          hitTypes: ['page_view']
+        }
+      ],
+      sessionIdProducer: async request => '123'
+    }
+    const r = { response: { statusCode: 204, variety: 'view' }, route: { path: '/test', fingerprint: 'A7a' }, ga: { view: viewSpy } }
+
+    it('should throw an error if the provided options are invalid', async () => {
+      expect.assertions(5)
       const server = {
         decorate: jest.fn(),
         ext: jest.fn()
       }
+      expect(indexPlugin.plugin.register).toBeDefined()
+      expect(typeof indexPlugin.plugin.register).toBe('function')
 
       await expect(indexPlugin.plugin.register(server, {})).rejects.toThrow('"propertySettings" is required')
+
       expect(server.decorate).toHaveBeenCalledTimes(0)
       expect(server.ext).toHaveBeenCalledTimes(0)
     })
 
-    it('should initiate Analytics if the provided options are correct', async () => {
-      expect(indexPlugin.plugin.register).toBeDefined()
-      expect(typeof indexPlugin.plugin.register).toBe('function')
+    it('should initiate Analytics - But it will not start tracking if options.trackAnalytics is set to false', async () => {
+      expect.assertions(7)
       const server = {
         decorate: jest.fn().mockImplementation(),
-        ext: jest.fn().mockImplementation()
+        ext: jest.fn().mockImplementation(async (e, c) => {
+          await c(r, { continue: "This is the best mock I ever wrote!" })
+        })
       }
-      const options = {
-        trackAnalytics: async () => false,
-        propertySettings: [
-          {
-            key: 'superSecretKey',
-            id: 'FakeId',
-            hitTypes: ['page_view']
-          }
-        ],
-        sessionIdProducer: async request => '123'
-      }
-      const viewSpy = jest.fn().mockImplementation(() => Promise.resolve())
+      options.trackAnalytics = async () => false
       analytics.mockImplementation(() => {
         return {
-          ga: {
-            view: viewSpy
-          }
+          ga: jest.fn().mockResolvedValueOnce({ view: viewSpy }),
         }
       })
 
       await indexPlugin.plugin.register(server, options)
+
       expect(server.decorate).toHaveBeenCalledTimes(1)
       expect(server.decorate).toHaveBeenCalledWith('request', 'ga', expect.any(Function), { apply: true })
       expect(server.ext).toHaveBeenCalledTimes(1)
       expect(server.ext).toHaveBeenCalledWith('onPreResponse', expect.any(Function))
       expect(analytics).toHaveBeenCalledTimes(1)
       expect(analytics).toHaveBeenCalledWith(options)
-    })
-
-    it('should track if trackAnalytics option is set to true', async () => {
-      const viewSpy = jest.fn().mockImplementation(() => {
-        return Promise.resolve()
-      })
-      const callback = jest.fn()
-      const options = {
-        trackAnalytics: async () => true,
-        propertySettings: [
-          {
-            key: 'superSecretKey',
-            id: 'FakeId',
-            hitTypes: ['page_view']
-          }
-        ],
-        sessionIdProducer: async request => '123'
-      }
-      const mockRequest = {
-        ga: {
-          view: viewSpy
-        },
-        response: {
-          statusCode: 204,
-          variety: 'view'
-        },
-        route: {
-          path: '/test'
-        }
-      }
-      await indexPlugin.handler(mockRequest, options, callback)
-      expect(callback).toHaveBeenCalledTimes(0)
-      expect(viewSpy).toHaveBeenCalledTimes(1)
-    })
-
-    it('should NOT track if trackAnalytics option is set to false', async () => {
-      const viewSpy = jest.fn().mockImplementation(() => {
-        return Promise.resolve()
-      })
-      const callback = jest.fn()
-      const options = {
-        trackAnalytics: async () => false,
-        propertySettings: [
-          {
-            key: 'superSecretKey',
-            id: 'FakeId',
-            hitTypes: ['page_view']
-          }
-        ],
-        sessionIdProducer: async request => '123'
-      }
-      const mockRequest = {
-        ga: {
-          view: viewSpy
-        },
-        response: {
-          statusCode: 204,
-          variety: 'view'
-        },
-        route: {
-          path: '/test'
-        }
-      }
-      await indexPlugin.handler(mockRequest, options, callback)
-      expect(callback).toHaveBeenCalledTimes(0)
       expect(viewSpy).toHaveBeenCalledTimes(0)
     })
 
-    it('should send exception event to GA on 50X responses', async () => {
+    it('should initiate Analytics if the provided options are valid - and start tracking page views', async () => {
+      expect.assertions(8)
       const viewSpy = jest.fn().mockImplementation(() => {
         return Promise.resolve()
       })
-      const callback = jest.fn()
+      const r = { response: { statusCode: 204, variety: 'view' }, route: { path: '/test', fingerprint: 'A7a' }, ga: { view: viewSpy } }
+      const server = {
+        decorate: jest.fn().mockImplementation(),
+        ext: jest.fn().mockImplementation(async (e, c) => {
+          await c(r, { continue: "I bet no one will read this!" })
+        })
+      }
       const options = {
         trackAnalytics: async () => true,
         propertySettings: [
@@ -146,21 +97,64 @@ describe('Index', () => {
         ],
         sessionIdProducer: async request => '123'
       }
-      const mockRequest = {
-        ga: {
-          view: viewSpy
-        },
-        response: {
-          statusCode: 500,
-          variety: 'view'
-        },
-        route: {
-          path: '/test'
+      analytics.mockImplementation(() => {
+        return {
+          ga: jest.fn().mockResolvedValueOnce({ view: viewSpy }),
         }
-      }
-      await indexPlugin.handler(mockRequest, options, callback)
-      expect(callback).toHaveBeenCalledTimes(0)
+      })
+
+      await indexPlugin.plugin.register(server, options)
+
+      expect(server.decorate).toHaveBeenCalledTimes(1)
+      expect(server.decorate).toHaveBeenCalledWith('request', 'ga', expect.any(Function), { apply: true })
+      expect(server.ext).toHaveBeenCalledTimes(1)
+      expect(server.ext).toHaveBeenCalledWith('onPreResponse', expect.any(Function))
+      expect(analytics).toHaveBeenCalledTimes(1)
+      expect(analytics).toHaveBeenCalledWith(options)
       expect(viewSpy).toHaveBeenCalledTimes(1)
+      expect(viewSpy).toHaveBeenCalledWith(r, { name: "pageview", params: { page_path: "/test", page_title: "A7a" } })
+    })
+    it('should send exception event to GA on internal server errors 50X', async () => {
+      expect.assertions(8)
+
+      const viewSpy = jest.fn().mockImplementation(() => {
+        return Promise.resolve()
+      })
+
+      const r = { response: { statusCode: 500, variety: 'view' }, route: { path: '/test', fingerprint: 'A7a' }, ga: { view: viewSpy } }
+      const server = {
+        decorate: jest.fn().mockImplementation(),
+        ext: jest.fn().mockImplementation(async (_e, c) => {
+          await c(r, { continue: "shame!" })
+        })
+      }
+      const options = {
+        trackAnalytics: async () => true,
+        propertySettings: [
+          {
+            key: 'superSecretKey',
+            id: 'FakeId',
+            hitTypes: ['page_view']
+          }
+        ],
+        sessionIdProducer: async request => '123'
+      }
+      analytics.mockImplementation(() => {
+        return {
+          ga: jest.fn().mockResolvedValueOnce({ view: viewSpy }),
+        }
+      })
+
+      await indexPlugin.plugin.register(server, options)
+
+      expect(server.decorate).toHaveBeenCalledTimes(1)
+      expect(server.decorate).toHaveBeenCalledWith('request', 'ga', expect.any(Function), { apply: true })
+      expect(server.ext).toHaveBeenCalledTimes(1)
+      expect(server.ext).toHaveBeenCalledWith('onPreResponse', expect.any(Function))
+      expect(analytics).toHaveBeenCalledTimes(1)
+      expect(analytics).toHaveBeenCalledWith(options)
+      expect(viewSpy).toHaveBeenCalledTimes(1)
+      expect(viewSpy).toHaveBeenCalledWith(r, { name: "exception", params: { error: 500, page_path: "/test", page_title: "A7a" } })
     })
   })
 })
